07e9f901024ca0eb263932a74f74a9b2aa3f8553
N0Whal3 <mihail12u4o@gmail.com>
2025-04-24 17:02:50 +0300

    added text indent, text align, exportMardown function, remake buttons array config, action function moved into separate state

--- markdown/src/App.jsx
+++ markdown/src/App.jsx
@@ -11,20 +11,12 @@ import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered';
 import FormatIndentIncreaseIcon from '@mui/icons-material/FormatIndentIncrease';
 import FormatIndentDecreaseIcon from '@mui/icons-material/FormatIndentDecrease';
-
-// import createInlineToolbarPlugin from 'draft-js-inline-toolbar-plugin';
-// import {
-//   ItalicButton,
-//   BoldButton,
-//   UnderlineButton,
-// } from 'draft-js-buttons';
-
-// import 'draft-js/dist/Draft.css';
-// import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
-
-// const inlineToolbarPlugin = createInlineToolbarPlugin();
-// const { InlineToolbar } = inlineToolbarPlugin;
-// const plugins = [inlineToolbarPlugin];
+import TableChartIcon from '@mui/icons-material/TableChart';
+import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft';
+import FormatAlignCenterIcon from '@mui/icons-material/FormatAlignCenter';
+import AlignHorizontalRightIcon from '@mui/icons-material/AlignHorizontalRight';
+import { stateToMarkdown } from 'draft-js-export-markdown'; //для экспорта разметки
+import './App.css'
 
 export default function App() {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -39,55 +31,101 @@ export default function App() {
   const currentStyle = editorState.getCurrentInlineStyle(); //style для inline элементов
   const currentBlockType = getCurrentBlock().getType();; //type для блочных элементов
 
+  const contentState = editorState.getCurrentContent(); //для экспорта разметки. Получаем весь текущий контент редактора
 
-  // const selection = editorState.getSelection();
-  // const content = editorState.getCurrentContent();
-  // const block = content.getBlockForKey(selection.getStartKey()); //получаем id блока (не строчных элементов)
+  // const markdown = stateToMarkdown(contentState); //для экспорта разметки. В заимпортированную функцию параметром передаём наш весь текущий контент
+  // console.log(markdown); //весь текст редактора с Markdown разметкой
+  // console.log(editorState.getCurrentContent().getPlainText()) //неформатированный текст (без Markdown)
 
-  const handleKeyCommand = (command, state) => {
-    const newState = RichUtils.handleKeyCommand(state, command);
-    if (newState) {
-      setEditorState(newState);
-      return 'handled';
-    }
-    return 'not-handled';
+
+  // попытка в кастомную функцию для получения текста с Markdown разметкой.
+  //Нужна т.к. ИЗ КОРОБКИ либа не поддерживает добавление отступов для inline элементов,
+  //По факту css отступы, сделанные паддингом, конвертятся в пробелы этой функцией
+  const exportToMarkdownWithIndent = () => {
+    const contentState = editorState.getCurrentContent();
+  
+    let markdown = stateToMarkdown(contentState);
+    const blocks = contentState.getBlocksAsArray();
+  
+    const lines = markdown.split('\n');
+  
+    const indentedMarkdown = lines.map((line, index) => {
+      const block = blocks[index];
+      if (!block) return line;
+  
+      const depth = block.getDepth();
+      const type = block.getType();
+      const isTextOrHeader = type === 'unstyled' || type.startsWith('header');
+  
+      if (isTextOrHeader && depth > 0) {
+        const indent = '  '.repeat(depth); // 2 пробела на каждый уровень
+        return indent + line;
+      }
+  
+      return line;
+    }).join('\n');
+  
+    return indentedMarkdown;
   };
+  
 
+  // console.log(exportToMarkdownWithIndent())
 
+  //функция для разметки комбинациями клавиш
+  // const handleKeyCommand = (command, state) => {
+  //   const newState = RichUtils.handleKeyCommand(state, command);
+  //   if (newState) {
+  //     setEditorState(newState);
+  //     return 'handled';
+  //   }
+  //   return 'not-handled';
+  // };
+  
+  //массив кнопок тулбара с конфигом
   const BUTTON_INLINE_STYLES = [
     {
       label: 'Жирный текст',
-      style: 'BOLD',
+      action: 'style',
+      actionValue: 'BOLD',
       icon: <FormatBoldIcon />,
     },
     {
       label: 'Курсив',
-      style: 'ITALIC',
+      action: 'style',
+      actionValue: 'ITALIC',
       icon: <FormatItalicIcon />,
     },
     {
       label: 'Подчёркнутый текст',
-      style: 'UNDERLINE',
+      action: 'style',
+      actionValue: 'UNDERLINE',
       icon: <FormatUnderlinedIcon />,
     },
     {
       label: 'Зачеркнутый текст',
-      style: 'STRIKETHROUGH',
+      action: 'style',
+      actionValue: 'STRIKETHROUGH',
       icon: <FormatStrikethroughIcon />
     },
     {
       label: 'Нумерованный список',
-      type: 'ordered-list-item',
+      action: 'type',
+      actionValue: 'ordered-list-item',
       icon: <FormatListNumberedIcon />
     },
     {
       label: 'Ненумерованный список',
-      type: 'unordered-list-item',
+      action: 'type',
+      actionValue: 'unordered-list-item',
       icon: <FormatListBulletedIcon />
     },
     {
       label: 'Список заголовков',
       selectOptions: [
+        {
+          optionTitle: 'Обычный',
+          selectType: 'unstyled'
+        },
         {
           optionTitle: 'Заголовок 1',
           selectType: 'header-one'
@@ -115,138 +153,186 @@ export default function App() {
       ]
     },
     {
-      label: 'Увеличить отступ',
-      action: 'increase-indent',
-      icon: <FormatIndentIncreaseIcon />
+      label: 'Уменьшить отступ',
+      action: 'action',
+      actionValue: 'decrease-indent',
+      icon: <FormatIndentDecreaseIcon />
     },
     {
-      label: 'Уменьшить отступ',
-      action: 'decrease-indent',
-      icon: <FormatIndentDecreaseIcon />
+      label: 'Увеличить отступ',
+      action: 'action',
+      actionValue: 'increase-indent',
+      icon: <FormatIndentIncreaseIcon />
+    },
+    // {
+    //   label: 'Добавить таблицу',
+    //   icon: <TableChartIcon/>
+    // },
+    {
+      label: 'Выравнивание по левому краю',
+      action: 'alignment',
+      actionValue: 'left',
+      icon: <AlignHorizontalLeftIcon />
+    },
+    {
+      label: 'Выравнивание по центру',
+      action: 'alignment',
+      actionValue: 'center',
+      icon: <FormatAlignCenterIcon />
+    },
+    {
+      label: 'Выравнивание по правому краю',
+      action: 'alignment',
+      actionValue: 'right',
+      icon: <AlignHorizontalRightIcon />
     },
   ];
 
-  //функция для работы с inline элементами (форматирование текста)
-  const toggleInlineStyle = (style) => {
-    setEditorState(RichUtils.toggleInlineStyle(editorState, style));
-    setTimeout(() => {
-      editor.current?.focus();
-    }, 0);
-  };
-
-    //функция для работы с block элементами (добавление блочных элементов)
-  const toggleBlockType = (type) => {
+  //функция для работы с Заголовками из Select
+  const toggleSelectHeadings = (type) => {
     setEditorState(RichUtils.toggleBlockType(editorState, type));
     setTimeout(() => {
       editor.current?.focus();
     }, 0);
   };
 
-    //функция для работы с actions элементами (экшены добавления отступов)
-  // const handleIndentAction = (action) => {
-  //   const targetedBlock = getCurrentBlock();
-  //   const targetedBlockDepth = targetedBlock.getDepth();
+  //функция для отступов и выравнивания текста
+  const blockStyleFn = (block) => {
+    const depth = block.getDepth();
+    const alignment = block.getData().get('text-align');
+    let className = `block-depth-${depth}`;
+    if (alignment) {
+      className += ` align-${alignment}`;
+    }
+    return className;
+  };
 
-  //   let newDepth = targetedBlockDepth;
-
-  //   if (action === 'increase-indent' && currentDepth < 4) {
-  //     newDepth = currentDepth + 1;
-  //   }
-  //   if (action === 'decrease-indent' && currentDepth > 0) {
-  //     newDepth = currentDepth - 1;
-  //   }
-
-  //   const newBlock = block.set('depth', newDepth);
-  //   const content = editorState.getCurrentContent();
-  //   const blockMap = content.getBlockMap();
-  //   const newBlockMap = blockMap.set(block.getKey(), newBlock);
-  // }
-
-  window.addEventListener('click', () => {
-    console.log(getCurrentBlock().getDepth())
-  })
-
-  const handleIndentAction = (action) => {
-    // console.log('check')
-    const block = getCurrentBlock(); // получаем текущий блок
-
-    // console.log(block)
-    const currentDepth = block.getDepth(); // его текущий отступ
-    let newDepth = currentDepth;
-
-    // console.log(newDepth)
+  //функция для выравнивания текста в конкретной строке
+  function applyBlockAlignment(editorState, alignment) {
+    const content = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+    const key = selection.getStartKey();
+    const block = content.getBlockForKey(key);
   
-    // определяем новый отступ в зависимости от действия
+    const newBlockData = block.getData().set('text-align', alignment);
+  
+    const newContentState = content.merge({
+      blockMap: content.getBlockMap().set(key, block.set('data', newBlockData)),
+    });
+  
+    return EditorState.push(editorState, newContentState, 'change-block-data');
+  }
+
+  //
+  const handleIndentAction = (action) => {
+    const block = getCurrentBlock(); // получаем текущий блок
+    const currentDepth = block.getDepth(); // его текущий отступ
+    let newDepth = currentDepth; //текущее значение отступа для использования в функции
+  
+    // определяем новый отступ в зависимости от действия (увеличить / уменьшить)
     if (action === 'increase-indent' && currentDepth < 4) {
       newDepth = currentDepth + 1;
     } else if (action === 'decrease-indent' && currentDepth > 0) {
       newDepth = currentDepth - 1;
     } else {
-      return; // ничего не меняем — выходим
-    }
-  
-    // создаём новый блок с обновлённым depth
+      return;
+    }  
     const newBlock = block.set('depth', newDepth);
   
-    // получаем текущий content и blockMap
     const content = editorState.getCurrentContent();
     const blockMap = content.getBlockMap();
-  
-    // заменяем старый блок на новый
     const newBlockMap = blockMap.set(block.getKey(), newBlock);
   
-    // создаём новый ContentState
     const newContentState = content.merge({
       blockMap: newBlockMap,
-      selectionAfter: editorState.getSelection(), // добавляем это!
+      selectionAfter: editorState.getSelection(),
     });
   
-    // пушим новый ContentState в EditorState
     const newEditorState = EditorState.push(
       editorState,
       newContentState,
       'adjust-depth'
     );
-  
-    // обновляем состояние редактора
     setEditorState(newEditorState);
+  };
+
+  // переменная со всеми функциями для разных типов элементов
+  const BUTTON_ACTIONS = {
+    style: (editorState, setEditorState, value) =>
+      setEditorState(RichUtils.toggleInlineStyle(editorState, value)), //для inline элементов
+  
+    type: (editorState, setEditorState, value) =>
+      setEditorState(RichUtils.toggleBlockType(editorState, value)), //для блочных элементов
+  
+    action: (editorState, setEditorState, value, getCurrentBlock) =>
+      handleIndentAction(value, editorState, setEditorState, getCurrentBlock), //отступы (работают только для СПИСКОВ. Из коробки не поддерживаются для иных элементов)
+                                                                              // при экспорте разметки отдельная функция парсит "костыльные" отступы и преобразует в пробелы
+  
+    alignment: (editorState, setEditorState, value) => { //выравнивание текста в строке
+      const newEditorState = applyBlockAlignment(editorState, value);
+      setEditorState(newEditorState);
+    }
+  };
+
+  //функция, принимающая конфиг каждой кнопки и выполняющая действие на основании полей action и actionValue
+  const executeBtnAction = (btnConfig) => {
+    const { action, actionValue } = btnConfig;
+    const handler = BUTTON_ACTIONS[action];
+    if (handler) {
+      handler(editorState, setEditorState, actionValue, getCurrentBlock);
+    }
   };
 
   return (
     <>
       <div style={{ marginBottom: 10, padding: '15px', display: 'flex', alignItems: 'center' }}>
       {
+        //маппинг кнопок
         BUTTON_INLINE_STYLES.map((btnConfig) => {
+          //функция для добавления цвета активным кнопкам (разметка, которая применяется к вводимому тексту)
+          const isActive = (() => {
+            switch (btnConfig.action) {
+              case 'style':
+                return currentStyle.has(btnConfig.actionValue);
+              case 'type':
+                return currentBlockType === btnConfig.actionValue;
+              case 'alignment':
+                const currentAlignment = getCurrentBlock().getData().get('text-align') || 'left';
+                return currentAlignment === btnConfig.actionValue;
+              default:
+                return false;
+            }
+          })();
 
-          const isActive = btnConfig.style
-          ? currentStyle.has(btnConfig.style)
-          : currentBlockType === btnConfig.type;
+          //функция дизейбла кнопок отступов
+          const toggleIndentButtonsActitity = () => {
+            if (btnConfig.actionValue === 'increase-indent') {
+              return getCurrentBlock().getDepth() === 4;
+            } else if (btnConfig.actionValue === 'decrease-indent') {
+              return getCurrentBlock().getDepth() === 0;
+            }
+            return false;
+          }
 
           return (
             !btnConfig.selectOptions ? (
               <IconButton
                 key={btnConfig.label}
-                title={isActive ? `Выключить ${btnConfig.label}` : `Включить ${btnConfig.label}`}
+                title={btnConfig.label}
+                disabled={toggleIndentButtonsActitity()}
                 sx={{
                   padding: '6px',
                   marginRight: '6px',
                   borderRadius: '50%',
-                  border: isActive ? '2px solid #4caf50' : '2px solid rgba(128, 128, 128, 0.5)',
-                  backgroundColor: isActive ? '#e8f5e9' : 'transparent',
+                  border: isActive ? '2px solid #4caf50' : '2px solid transparent',
+                  // backgroundColor: isActive ? '#e8f5e9' : 'transparent',
                   transition: 'all 0.2s ease-in-out',
                 }}
                 color={isActive ? 'success' : 'default'}
                 onMouseDown={(e) => {
                   e.preventDefault();
-                  if (btnConfig.style) {
-                    toggleInlineStyle(btnConfig.style)
-                  } else if (btnConfig.type) {
-                    toggleBlockType(btnConfig.type)
-                  } else if (btnConfig.action) {
-                    handleIndentAction(btnConfig.action)
-                  };
-                  //   ? toggleInlineStyle(btnConfig.style)
-                  // : toggleBlockType(btnConfig.type);
+                  // executeBtnAction(btnConfig);
+                  executeBtnAction(btnConfig);
               }}
             >
               {btnConfig.icon}
@@ -255,21 +341,21 @@ export default function App() {
               <Select
                 key={btnConfig.label}
                 value={currentBlockType}
-                displayEmpty
                 onChange={(e) => {
-                  toggleBlockType(e.target.value);
+                  toggleSelectHeadings(e.target.value);
                 }}
                 size="small"
                 sx={{ minWidth: 160, marginRight: '6px' }}
                 color={isActive ? 'success' : 'default'}
                 title={btnConfig.label}
               >
-                {btnConfig.selectOptions.map((option, index) => (
+                {/* маппинг options для select'a с заголовками. Slice'om НЕ рендерит первый option из массива */}
+                {btnConfig.selectOptions.slice(1).map((option, index) => (
                   <MenuItem value={option.selectType} key={option.optionTitle}>
                     <Typography
                       variant={`h${option.optionTitle.slice(-1)}`}
                       sx={{
-                        fontSize: `${21 - (index + 2)}px`
+                        fontSize: `${20 - (index + 1)}px`
                       }}
                     >
                       {option.optionTitle}
@@ -288,8 +374,10 @@ export default function App() {
         editorState={editorState}
         onChange={setEditorState}
         ref={editor}
-        handleKeyCommand={handleKeyCommand}
+        // handleKeyCommand={handleKeyCommand} //для добавления разметки через комбинации клавиш
         style={{height: '100%'}}
+        blockStyleFn={blockStyleFn}
+        // readOnly={true} //делаем редактор только для чтения
       />
     </div>
     </>
